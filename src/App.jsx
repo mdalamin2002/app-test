54052ffe313a1c84b48f9505d5d100c91b7f851b
unknown <sabrinaboby786@gmail.com>
2025-09-13 21:41:03 +0600

    added

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ function App() {
       <div> alamin</div>
       <h1>this is an edit in feature 1     branch </h1>
       <h1>this is an edit in feature 2     branch </h1>
+      <h2>hi</h2>
     </>
   )
 }
