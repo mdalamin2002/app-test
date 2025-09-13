f5ee2ea5eb9c4b4456f24064a8e9a260a349bac8
Kausar Hossain Bidyut <khbidyut31@gmail.com>
2025-09-14 00:28:49 +0600

    5th number

--- src/footer.jsx
+++ src/footer.jsx
@@ -4,6 +4,7 @@ const footer = () => {
     return (
         <div>
             <h1>footer</h1>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi tempora quibusdam expedita corporis fugiat, perspiciatis eligendi a quam dolores, sunt iusto maiores incidunt ratione adipisci natus voluptatibus praesentium perferendis quod!
         </div>
     );
 };
